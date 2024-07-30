23ca25bce1bd45282164d3501178f56729972a5c
bb8ZgjUjU <joharrywork@gmail.com>
2024-07-30 22:11:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,13 @@
 
 
     <?php
-        $name = "Joharry";
-        echo $name;
+        echo $_SERVER["DOCUMENT_ROOT"];
+        echo "<br>";
+        echo $_SERVER["PHP_SELF"];
+        echo "<br>";
+        echo $_SERVER["SERVER_NAME"];
+        echo "<br>";
+        echo $_SERVER["REQUEST_METHOD"];
     ?>
 
     
