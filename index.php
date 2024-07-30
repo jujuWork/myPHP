b7f4aebf90811e54f28f31ae97d1a2dacacdde72
bb8ZgjUjU <joharrywork@gmail.com>
2024-07-30 22:21:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,7 @@
 
 
     <?php
-        echo $_SERVER["DOCUMENT_ROOT"];
-        echo "<br>";
-        echo $_SERVER["PHP_SELF"];
-        echo "<br>";
-        echo $_SERVER["SERVER_NAME"];
-        echo "<br>";
-        echo $_SERVER["REQUEST_METHOD"];
+        echo $_GET["name"];
     ?>
 
     
