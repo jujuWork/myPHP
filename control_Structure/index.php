6284e1fb6d8d254a03add45a383317f44678f534
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-01 22:52:20 +0900

    Create index.php

---
+++ control_Structure/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    
+    // IF ELSE STATEMENT
+
+    $bool = true;
+    $a = 1;
+    $b = 4;
+
+    if ($a < $b && !$bool) {
+        echo "First condition is true!";
+    } else if ($a < $b && !$bool) {
+        echo "Second Condition is true!";
+    } else {
+        echo "None of the Conditions were true";
+    }
+    
+    ?>
+
+</body>
+</html>
