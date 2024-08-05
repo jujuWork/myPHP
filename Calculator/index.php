b818b35545a8459353d89476c02ac41a480c13e8
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-05 22:46:05 +0900

    08052024

--- Calculator/index.php
+++ Calculator/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculator</title>
+
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get"></form>
+
 </body>
 </html>
