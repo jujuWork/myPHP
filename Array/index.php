a044da40d366da114521feda1fa05bf6abf0c64f
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-06 23:20:46 +0900

    Update index.php

--- Array/index.php
+++ Array/index.php
@@ -9,12 +9,24 @@
     
     <?php
         $fruits = [
-            "Apple", 
-            "Banana", 
-            "Cherry"
+            "Apple", // 0
+            "Banana", // 1
+            "Cherry" // 2
         ];
 
+        $fruits [] = "Orange";
         echo $fruits[2];
+
+        $tasks = [
+            "trash" => "Joharry",
+            "laundry" => "Rina",
+            "play" => "Grey"
+        ];
+
+        echo $tasks["play"];
+
+        array_splice($fruits, 2, 0, $tasks);
+        print_r($fruits);
     ?>
 
 </body>
