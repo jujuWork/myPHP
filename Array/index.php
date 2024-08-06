a4a1c155c1068d19d71c095b88de12b737de4009
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-06 22:57:00 +0900

    Create index.php

---
+++ Array/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+</head>
+<body>
+    
+    <?php
+        $fruits = [
+            "Apple", 
+            "Banana", 
+            "Cherry"
+        ];
+
+        echo $fruits[2];
+    ?>
+
+</body>
+</html>
