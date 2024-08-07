99c3f48d1410c0b5c0641393b1a382a4c56543ec
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-07 23:08:46 +0900

    Update index.php

--- Built_In/index.php
+++ Built_In/index.php
@@ -9,13 +9,42 @@
     
     <?php
 
-    $string = "Hello World!";
+    /*
+
+    ($string = "Hello World!";
 
     echo strlen($string);
     echo strpos($string, "o");
     echo str_replace("World!", "Joharry", $string);
     echo substr($string, 2, -2);
     print_r(explode(" ", $string));
+    
+    
+
+    // MATH
+
+    $number = -5.5;
+
+    echo abs($number);
+    echo round($number);
+    echo pow(2, 3);
+    echo sqrt(16);
+    echo rand(1, 100);
+
+    */
+
+    // ARRAY
+
+    $array = ["apple", "banana", "cherry"];
+
+    echo count($array);
+    echo is_array($array);
+
+    array_push($array, "Kiwi");
+    print_r($array);
+
+    array_pop($array);
+    print_r($array);
 
 
     ?>
