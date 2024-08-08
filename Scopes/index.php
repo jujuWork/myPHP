cd905f65ff63cafbe96faf31d4f3f1ce5f470807
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-08 22:27:51 +0900

    Create index.php

---
+++ Scopes/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scopes</title>
+</head>
+<body>
+    
+    <?php
+
+    $test = "Joharry";
+
+    function myFunction()
+    {
+        global $test;
+
+        $localVar = "Hello, World!";
+
+        return $test;
+    }
+
+    echo myFunction();
+
+    ?>
+
+</body>
+</html>
