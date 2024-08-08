c7e993477402375303217652878dc753594e163f
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-08 23:19:01 +0900

    Update index.php

--- Loops/index.php
+++ Loops/index.php
@@ -11,12 +11,36 @@
 
     // FOR LOOP
 
-    for($i = 0; $i <= 10; $i++) {
+    /*
+    for ($i = 0; $i <= 10; $i++) {
         echo "This is iteration number " . $i . "<br>";
     }
+    */
 
     // WHILE LOOP
+    /*
+    $test = 0;
+    while ($test < 10) {
+        echo $test;
+        $test++;
+    }
+    */
 
+    // DO WHILE
+    /*
+    $test = 1;
+    do {
+        echo $test;
+        $test++;
+    } while ($test < 10);
+    */
+
+    //FOR EACH
+    $fruits = array("Apple" => "Red", "Banana" => "Yellow", "Orange" => "Orange");
+
+    foreach ($fruits as $fruit => $color) {
+        echo "This is a " . $fruit . ", that has a color of " . $color . "<br>";
+    }
 
     ?>
 
