83836a9100537773da3504684fb5ecec426b1fe3
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-26 00:00:29 +0900

    08252024

---
+++ Add & Delete/includes/userdelete.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+
+    try {
+        require_once "dbh.inc.php";
+
+        $query = "DELETE FROM users WHERE username = :username AND pwd = :pwd;";
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pwd", $pwd);
+
+        $stmt->execute();
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../index.php");
+
+        exit();
+    } catch (PDOException $e) {
+        die("Query Failed: " . $e->getMessage());
+    }
+}
+else {
+    header("Location: ../index.php");
+}
