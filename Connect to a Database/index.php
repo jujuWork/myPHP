4d3f1ebb885baa248432fa59e200109e5f5b7652
bb8ZgjUjU <joharrywork@gmail.com>
2024-08-25 22:59:20 +0900

    08252024

---
+++ Connect to a Database/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="includes/index.css">
+</head>
+<body>
+    
+    <h3>SIGN UP</h3>
+
+    <form action="includes/formhandles.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+
+        <button>Sign Up</button>
+    </form>
+
+
+</body>
+</html>
