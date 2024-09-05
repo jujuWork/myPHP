211a4c1ba352825f439b8b0c0e49f5584062af73
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-09-05 23:23:45 +0900

    090524

--- Classes/car.php
+++ Classes/car.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "hello world";
+class Car 
+{
+    //Properties / Fields
+
+    private $brand;
+    private $color;
+    public $vehicleType = "car";
+
+    //Contructor
+
+    public function __construct($brand, $color) //THIS IS NOT THE SAME AS THE PROPERTIES ABOVE( LIKE A PLACEHOLDER ONLY)
+    {
+      $this->brand = $brand; //brand(properties) ---- $brand(placeholder)
+      $this->color = $color;  //color(properties) ---- $color(placeholder)
+    }
+
+    //Method (accessing private properties)
+
+    public function getCarInfo()
+    {
+        return "Brand: " . $this->brand . ", Color: " . $this->color;   
+    }
+}
+
+$car01 = new Car("Toyota", "gray");
+echo $car01->getCarInfo();
+echo $car01->vehicleType();
